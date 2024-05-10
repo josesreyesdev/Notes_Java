a8467c07d9083f3c8597b0e05388e6e3c4213b43
Jose SR <josesanchez09190@gmail.com>
2024-05-10 01:37:41 -0600

    search movie

--- src/com/alura/movies/main/MainWithSearch.java
+++ src/com/alura/movies/main/MainWithSearch.java
@@ -5,27 +5,34 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class MainWithSearch {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingresa el nombre de la pelicula: ");
+        var movieName = scanner.nextLine().trim();
+        var movieResultName = movieName.replace(" ", "+%26+");
+
+        String url = "https://www.omdbapi.com/?t=" + movieResultName +"&apikey=afd234123";
+
+        // & = %26
         // REQUEST
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://www.omdbapi.com/?t=batman&apikey=ax1234123"))
+                .uri(URI.create(url))
                 .build();
-        /*client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
-                .thenApply(HttpResponse::body)
-                .thenAccept(System.out::println)
-                .join(); */
-
 
         // RESPONSE
         try {
             HttpResponse<String> response = client
                     .send(request, HttpResponse.BodyHandlers.ofString());
 
-            System.out.println("Response: " + response.body());
+            System.out.println("Response => " + response.body());
         } catch (IOException | InterruptedException e) {
+            System.out.println("Error al intentar hacer la solicitud");
             throw new RuntimeException(e);
         }
     }
